e61328199f23a75dbea3ab6788efbc95bd8a30ff
JaxMachine <newtradesofjack@gmail.com>
2023-12-17 22:39:47 -0600

    Part One

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const taskSubmit = document.querySelector("form");
 const taskList = document.querySelector("#task-list");
 const toDo = document.querySelector("#new-task");
 
+const tasks = document.getElementById("#taskEntry");
+
 //Task List Creation and Removal
 taskList.addEventListener("click", function (e) {
   if (e.target.tagName === "BUTTON") {
@@ -15,13 +17,12 @@ taskList.addEventListener("click", function (e) {
 taskSubmit.addEventListener("submit", function (e) {
   e.preventDefault();
   const newTaskInput = document.querySelector("#new-task");
-  console.log(newTaskInput.value);
   const newLi = document.createElement("li");
   const newButton = document.createElement("Button");
   newLi.innerText = newTaskInput.value;
   newButton.innerText = "Remove Task";
-
+  newSpan = document.createElement("span");
   newLi.append(newButton);
   taskList.append(newLi);
-  form.rest();
+  taskSubmit.reset();
 });
